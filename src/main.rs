225480cce3b90c1f07f79ea39008b4257000b82e
argothiel <13932353+argothiel@users.noreply.github.com>
2026-08-16 16:56:56 +0200

    Factor into functions

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,22 @@
 use std::env;
 
+struct Arguments<'a> {
+    source: &'a str,
+    target: &'a str,
+}
+
+fn parse_config(args: &[String]) -> Arguments<'_> {
+    // mount.isomorfs SOURCE TARGET [-sfnv] [-N namespace] [-o options] [-t type.subtype]
+    Arguments {source : &args[1], target : &args[2]}
+}
+
+fn process(args: &[String]) {
+    let config = parse_config(args);
+
+    println!("{} => {}", config.source, config.target);
+}
+
 fn main() {
     let args: Vec<String> = env::args().collect();
-
-    let source = &args[1];
-    let target = &args[2];
-
-    println!("{source} => {target}");
+    process(&args);
 }
