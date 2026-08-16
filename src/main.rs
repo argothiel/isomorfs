d5d4afa3e7afc1253c70eafa55168d9c47deb543
argothiel <13932353+argothiel@users.noreply.github.com>
2026-08-16 17:33:55 +0200

    Add tests

--- src/main.rs
+++ src/main.rs
@@ -24,3 +24,74 @@ fn main() {
     let args: Vec<String> = env::args().collect();
     process(&args);
 }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn parses_source_and_target_paths() {
+        let command = [
+            "mount.isomorfs",
+            "/images/installer image.iso",
+            "/mnt/iso mount",
+        ];
+        let args = command.map(str::to_string);
+
+        let config = parse_config(&args);
+
+        assert_eq!(config.source, Path::new("/images/installer image.iso"));
+        assert_eq!(config.target, Path::new("/mnt/iso mount"));
+    }
+
+    #[test]
+    fn ignore_invalid_arguments() {
+        let command = [
+            "INVALID",
+            "/images/installer image.iso",
+            "/mnt/iso mount",
+            "foo",
+            "-bar",
+        ];
+        let args = command.map(str::to_string);
+
+        let config = parse_config(&args);
+
+        assert_eq!(config.source, Path::new("/images/installer image.iso"));
+        assert_eq!(config.target, Path::new("/mnt/iso mount"));
+    }
+
+    #[test]
+    fn ignore_options() {
+        let command = [
+            "mount.isomorfs",
+            "/images/installer image.iso",
+            "/mnt/iso mount",
+            "-s",
+            "-f",
+            "-n",
+            "-v",
+            "-N",
+            "myns",
+            "-o",
+            "ro,nodev",
+            "-t",
+            "isomorfs.subtype",
+        ];
+        let args = command.map(str::to_string);
+
+        let config = parse_config(&args);
+
+        assert_eq!(config.source, Path::new("/images/installer image.iso"));
+        assert_eq!(config.target, Path::new("/mnt/iso mount"));
+    }
+
+    #[test]
+    #[should_panic(expected = "index out of bounds")]
+    fn panic_at_wrong_number_of_arguments() {
+        let command = ["INVALID"];
+        let args = command.map(str::to_string);
+
+        parse_config(&args);
+    }
+}
