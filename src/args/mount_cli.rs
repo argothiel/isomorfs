d89fa17b88ddd775b285a90976fff89cb96b4655
argothiel <13932353+argothiel@users.noreply.github.com>
2026-08-21 03:00:34 +0200

    Implement StylePreservingStr for easy StyledStr processing

--- src/args/mount_cli.rs
+++ src/args/mount_cli.rs
@@ -1,11 +1,15 @@
-use clap::builder::Str;
+use clap::builder::{Str, StyledStr};
 use clap::{Arg, Command, Id};
+use std::fmt::Write;
 
-pub fn render_usage_with_help(command: &Command) -> (String, String) {
-    HelpRenderer::new(command).render_usage_with_help()
+pub fn render_usage_with_help(command: &Command) -> (StyledStr, StyledStr) {
+    let (usage, help) = HelpRenderer::new(command).render_usage_with_help();
+    (usage.into(), help.into())
 }
 
 const SHORT_PLACEHOLDER: &str = "    ";
+
+struct StylePreservingStr(StyledStr);
 
 struct SplitArguments<'a> {
     bin: &'a str,
@@ -16,8 +20,38 @@ struct SplitArguments<'a> {
 }
 
 struct HelpRenderer<'a> {
-    _command: &'a Command,
+    command: &'a Command,
     arguments: SplitArguments<'a>,
+}
+
+impl std::fmt::Display for StylePreservingStr {
+    fn fmt(&self, formatter: &mut std::fmt::Formatter) -> std::fmt::Result {
+        self.0.ansi().fmt(formatter)
+    }
+}
+
+impl Write for StylePreservingStr {
+    fn write_str(&mut self, s: &str) -> std::fmt::Result {
+        Write::write_str(&mut self.0, s)
+    }
+}
+
+impl From<String> for StylePreservingStr {
+    fn from(value: String) -> Self {
+        Self(StyledStr::from(value))
+    }
+}
+
+impl From<StylePreservingStr> for StyledStr {
+    fn from(value: StylePreservingStr) -> Self {
+        value.0
+    }
+}
+
+impl StylePreservingStr {
+    fn new() -> StylePreservingStr {
+        StylePreservingStr(StyledStr::new())
+    }
 }
 
 impl<'a> SplitArguments<'a> {
@@ -47,104 +81,108 @@ impl<'a> SplitArguments<'a> {
 impl<'a> HelpRenderer<'a> {
     fn new(command: &'a Command) -> Self {
         HelpRenderer {
-            _command: command,
+            command,
             arguments: get_split_arguments(command),
         }
     }
 
-    fn render_usage_with_help(&self) -> (String, String) {
+    fn render_usage_with_help(&self) -> (StylePreservingStr, StylePreservingStr) {
         let usage = render_usage_without_title(&self.arguments);
 
         let arguments_help = self.render_table_rows(&self.arguments.positionals);
         let all_options_help = self.render_table_rows(&self.arguments.all_options);
-        let help = format!(
-            "Usage: {usage}
+        let help = StylePreservingStr::from(format!(
+            "{}Usage{}: {usage}
 
 Arguments:
 {arguments_help}
 
 Options:
-{all_options_help}\n"
-        );
+{all_options_help}\n",
+            self.command.get_styles().get_usage().render(),
+            self.command.get_styles().get_usage().render_reset(),
+        ));
 
         (usage, help)
     }
 
-    fn render_short(&self, argument: &Arg) -> String {
+    fn render_short(&self, argument: &Arg) -> StylePreservingStr {
         if let Some(s) = argument.get_short() {
-            return format!("-{s}");
+            return StylePreservingStr::from(format!("-{s}"));
         }
         if argument.get_long().is_some() {
-            return SHORT_PLACEHOLDER.to_owned();
+            return StylePreservingStr::from(SHORT_PLACEHOLDER.to_owned());
         }
-        String::new()
+        StylePreservingStr::new()
     }
 
-    fn render_long(&self, argument: &Arg) -> String {
+    fn render_long(&self, argument: &Arg) -> StylePreservingStr {
         if let Some(l) = argument.get_long() {
             if argument.get_short().is_some() {
-                return ", --".to_string() + l;
+                return StylePreservingStr::from(", --".to_string() + l);
             }
-            return "--".to_string() + l;
+            return StylePreservingStr::from("--".to_string() + l);
         }
-        String::new()
+        StylePreservingStr::new()
     }
 
-    fn render_suffix(&self, argument: &Arg) -> String {
+    fn render_suffix(&self, argument: &Arg) -> StylePreservingStr {
         if argument.is_positional() {
-            return argument.get_id().to_string();
+            return StylePreservingStr::from(argument.get_id().to_string());
         }
         if argument.get_num_args().unwrap_or_default().takes_values() {
             let names = argument.get_value_names().unwrap();
-            return " ".to_string()
-                + names
-                    .iter()
-                    .map(Str::as_str)
-                    .collect::<Vec<_>>()
-                    .join(" ")
-                    .as_str();
+            return StylePreservingStr::from(
+                " ".to_string()
+                    + names
+                        .iter()
+                        .map(Str::as_str)
+                        .collect::<Vec<_>>()
+                        .join(" ")
+                        .as_str(),
+            );
         }
-        " ".to_string()
+        StylePreservingStr::from(" ".to_string())
     }
 
-    fn render_description(&self, argument: &Arg) -> String {
-        argument.get_help().unwrap_or_default().to_string()
+    fn render_description(&self, argument: &Arg) -> StylePreservingStr {
+        StylePreservingStr::from(argument.get_help().unwrap_or_default().to_string())
     }
 
-    fn render_label(&self, argument: &Arg) -> String {
+    fn render_label(&self, argument: &Arg) -> StylePreservingStr {
         let short = self.render_short(argument);
         let long = self.render_long(argument);
         let suffix = self.render_suffix(argument);
-        format!("  {short}{long}{suffix}")
+        StylePreservingStr::from(format!("  {short}{long}{suffix}"))
     }
 
-    fn render_row(&self, argument: &Arg, length: usize) -> String {
-        format!(
+    fn render_row(&self, argument: &Arg, length: usize) -> StylePreservingStr {
+        StylePreservingStr::from(format!(
             "{:length$}  {}",
             self.render_label(argument),
             self.render_description(argument)
-        )
+        ))
     }
 
-    fn render_table_rows(&self, arguments: &[&Arg]) -> String {
+    fn render_table_rows(&self, arguments: &[&Arg]) -> StylePreservingStr {
         if arguments.is_empty() {
-            return "".to_string();
+            return StylePreservingStr::from("".to_string());
         }
         let max_length = arguments
             .iter()
-            .map(|arg| self.render_label(arg).len())
+            .map(|arg| self.render_label(arg).to_string().len())
             .max()
             .unwrap();
 
         let rendered_rows: Vec<String> = arguments
             .iter()
-            .map(|row| self.render_row(row, max_length))
+            .map(|row| self.render_row(row, max_length).to_string())
             .collect();
-        rendered_rows.join("\n")
+        StylePreservingStr::from(rendered_rows.join("\n"))
     }
 }
 
-fn render_usage_value_option(arg: &Arg) -> String {
+fn render_usage_value_option(arg: &Arg) -> StylePreservingStr {
     let short = arg.get_short().unwrap();
 
     let value = arg
@@ -152,7 +190,7 @@ fn render_usage_value_option(arg: &Arg) -> String {
         .and_then(<[Str]>::first)
         .map(Str::as_str)
         .unwrap_or("VALUE");
-    format!("[-{short} {value}]")
+    StylePreservingStr::from(format!("[-{short} {value}]"))
 }
 
 fn get_split_arguments(command: &Command) -> SplitArguments<'_> {
@@ -171,7 +209,7 @@ fn get_split_arguments(command: &Command) -> SplitArguments<'_> {
     split_arguments
 }
 
-fn render_usage_without_title(split_arguments: &SplitArguments) -> String {
+fn render_usage_without_title(split_arguments: &SplitArguments) -> StylePreservingStr {
     let SplitArguments {
         bin,
         short_options,
@@ -185,6 +223,7 @@ fn render_usage_without_title(split_arguments: &SplitArguments) -> String {
         .iter()
         .copied()
         .map(render_usage_value_option)
+        .map(|option| StylePreservingStr::to_string(&option))
         .collect::<Vec<_>>()
         .join(" ");
     let joined_positionals: String = positionals
@@ -195,5 +234,10 @@ fn render_usage_without_title(split_arguments: &SplitArguments) -> String {
         .collect::<Vec<_>>()
         .join(" ");
 
-    format!("{bin} {joined_positionals} [-{joined_flags}] {joined_options}")
+    let mut writer = StylePreservingStr::new();
+    let _ = write!(
+        writer,
+        "{bin} {joined_positionals} [-{joined_flags}] {joined_options}"
+    );
+    writer
 }
